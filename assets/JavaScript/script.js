a31106b5692cf11c7fd1f54997f79f819ec61a5a
StevenTraboulay <khaleeltraboulay@gmail.com>
2020-10-11 19:43:48 -0400

    all code in but some broken issues

--- assets/JavaScript/script.js
+++ assets/JavaScript/script.js
@@ -4,19 +4,18 @@ function getSearchVal() {
   searchWeather(searchValue);
   makeRow(searchValue);
 }
-
 //cretae new row in page in search
 function makeRow(searchValue) {
-  var liEl = document.createElement("li");
+  var liEl = document.createElement("li")
   liEl.classList.add("list-group-item", "list-group-item-action");
   var text = searchValue;
   liEl.textContent = text;
   var historyEl = document.querySelector(".history");
-  console.log(event.target);
+  //console.log(event.target);
   historyEl.onclick = function () {
-    console.log(event.target.tagName);
+    //console.log(event.target.tagName);
     if (event.target.tagName == "LI") {
-      searchWeather(event.target.textContent);
+      searchWeather(event.target.textContent)
     }
   };
   historyEl.appendChild(liEl);
@@ -30,8 +29,8 @@ function searchWeather(searchValue) {
       "&appid=f9536bc7a9491fe897bd179cb356d42a&units=metric"
   ).then(function (response) {
     return response.json();
-  });
-  then(function (data) {
+  })
+  .then(function (data) {
     //content clear
     todayEl = document.querySelector("#today");
     todayEl.textContent = " ";
@@ -58,7 +57,7 @@ function searchWeather(searchValue) {
       "src",
       "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png"
     );
-//appending info to cards
+    //appending info to cards
     titleEl.appendChild(imgEl);
     cardBodyEl.appendChild(titleEl);
     cardBodyEl.appendChild(tempEl);
@@ -69,39 +68,108 @@ function searchWeather(searchValue) {
 
     getForecast(searchValue);
     getUVIndex(data.coord.lat, data.coord.lon);
-  })
+  });
 }
 
 //forecast func
-function getForecast(searchValue){
-    fetch(
-        "http://api.openweathermap.org/data/2.5/weather?q=" +
-          searchValue +
-          "&appid=f9536bc7a9491fe897bd179cb356d42a&units=metric"
-      ).then(function(response){
-          return response.json();
-      })
-      .then(function(data){
-          console.log(data)
-          var forecastEl = document.querySelector("#forecast");
-          forecastEl.innerHTML = "<h4 class=\"mt\">5-day Forecast:</h4>";
-          forecastRowEl = document.createElement("div");
-          forecastRowEl.className = "\"row\"";
+function getForecast(searchValue) {
+  fetch(
+    "http://api.openweathermap.org/data/2.5/weather?q=" +
+      searchValue +
+      "&appid=f9536bc7a9491fe897bd179cb356d42a&units=metric"
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      var forecastEl = document.querySelector("#forecast");
+      forecastEl.innerHTML = '<h4 class="mt">5-day Forecast:</h4>';
+      forecastRowEl = document.createElement("div");
+      forecastRowEl.className = '"row"';
 
-          //loop over all forecasts in xxxx hours
+      //loop over all forecasts in xxxx hours
 
-          for (var i = 0; i <data.list.length; i++){
+      for (var i = 0; i < data.list.length; i++) {
+        //forecasts at 3pm
+        if (data.list[i].dt_txt.indexOf("15:00:00)") !== -1) {
+          //html for card
+          var colEl = document.createElement("div");
+          colEl.classList.add("col-md-2");
+          var cardEl = document.createElement("div");
+          cardEl.classList.add("card", "bg-primary", "text-white");
+          var windEl = document.createElement("P");
+          windEl.classList.add("card-text");
+          windEl.textContent =
+            " Wind speed: " + data.list[i].wind.speed + "kmph";
+          var humidityEl = document.createElement("p");
+          humidityEl.classList.add("card-text");
+          humidityEl.textContent =
+            "Humidity: " + data.list[i].main.humdity + " %";
+          var bodyEl = document.createElement("div");
+          bodyEl.classList.add("card-body", "p-2");
+          var titleEl = document.createElement("h5");
+          titleEl.classList.add("card-title");
+          titleEl.textContent = new Date(
+            data.list[i].dt_txt
+          ).toLocaleDateString();
+          var imgEl = document.createElement("img");
+          imgEl.setAttribute(
+            "src",
+            "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png"
+          );
+          var p1El = document.createElement("p");
+          p1El.classList.add("card-text");
+          p1El.textContent = "Temp: " + data.list[i].main.temp_max + "°C";
+          var p2El = document.createElement("p");
+          p2El.classList.add("card-text");
+          p2El.textContent = "Humidity: " + data.list[i].main.humidity + "%";
 
-            //forecasts at 3pm
-            if (data.list[i].dt_txt.indexOf("15:00:00)") !== -1){
-
-                //html for card
-                var colEl = document.createElement("div");
-                colEl.classList.add("col-md-2");
-                var cardEl = document.createElement("div");
-                cardEl.classList.add("card", "bg-primary", "text-white");
-            }
-          }
-      })
+          colEl.appendChild(cardEl);
+          bodyEl.appendChild(titleEl);
+          bodyEl.appendChild(imgEl);
+          bodyEl.appendChild(windEl);
+          bodyEl.appendChild(humidityEl);
+          bodyEl.appendChild(p1El);
+          bodyEl.appendChild(p2El);
+          cardEl.appendChild(bodyEl);
+          forecastEl.appendChild(colEl);
+        }
+      }
+    });
 }
 
+function getUVIndex(lat, lon) {
+  fetch(
+    "http://api.openweathermap.org/data/2.5/weather?lat=" +
+      lat +
+      "&lon=" +
+      lon +
+      "&appid=f9536bc7a9491fe897bd179cb356d42a"
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      var bodyEl = document.querySelector(".card-body");
+      var uvEl = document.createElement("p");
+      uvEl.textContent = "UV Index: ";
+      var buttonEl = document.createElement("span");
+      buttonEl.classList.add("btn", "btn-sm");
+      buttonEl.innerHTML = data.value;
+
+      if (data.value < 3) {
+        buttonEl.classList.add("btn-success");
+      } else if (data.value < 7) {
+        buttonEl.classList.add("btn-warning");
+      } else {
+        buttonEl.classList.add("btn-dange");
+      }
+      bodyEl.appendChild(uvEl);
+      uvEl.appendChild(buttonEl);
+    });
+}
+
+document
+  .querySelector("#search-button")
+  .addEventListener("click", getSearchVal);
